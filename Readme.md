2f04bcf0c221934f101e0244f6bdb803af28a9d2
Josh Padnick <josh.padnick@gmail.com>
2015-05-31 11:36:48 -0700

    Added intelligent AMI name based on timestamp.

--- Readme.md
+++ Readme.md
@@ -28,3 +28,15 @@ Common USA regions are:
 - North Virginia = `us-east-1`
 - North California = `us-west-1`
 - Oregon = `us-west-2` 
+
+## Usage
+
+### Create an AMI
+```
+ec2-snapper create --name=ami-2 --instance-id=i-c724be30 --dry-run --no-reboot
+```
+
+### Delete AMIs older than X days
+```
+ec2-snapper delete --instance-id=i-c724b30 --min-age=30d
+```
--- main.go
+++ main.go
@@ -2,33 +2,38 @@ package main
 
 import (
 	"fmt"
+	"time"
 	//"github.com/awslabs/aws-sdk-go/aws"
 	"github.com/awslabs/aws-sdk-go/service/ec2"
 )
 
 func main() {
-	fmt.Println("Hello Josh!")
 
 	// Create an EC2 service object; AWS region is picked up from the "AWS_REGION" env var.
 	svc := ec2.New(nil)
 
+	// Generate a nicely formatted timestamp for right now
+	const layout = "2006-01-02 at 15-04-05 (MST)"
+	t := time.Now()
+
 	// Create the AMI Snapshot
 	// Todo: Capture these vals from command-line args
 	name := "AMI-2"
+	name += " " + t.Format(layout)
 	instanceId := "i-c724be30"
 	dryRun := false
 	noReboot := true
+
+	fmt.Printf("==> Creating AMI for %s...\n", instanceId)
 
 	resp, err := svc.CreateImage(&ec2.CreateImageInput{
 		Name: &name,
 		InstanceID: &instanceId,
 		DryRun: &dryRun,
 		NoReboot: &noReboot })
-
 	if err != nil {
 		panic(err)
 	}
 
-	// Todo: Print out nice message
-	fmt.Println("ImageID = ",*resp.ImageID)
+	fmt.Printf("==> Created %s named \"%s\"\n",*resp.ImageID,name)
 }
