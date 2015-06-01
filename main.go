40f7239f2232c93ade0e7c8393de89251f2d9ed6
Josh Padnick <josh.padnick@gmail.com>
2015-06-01 00:14:44 -0700

    Re-add main.go

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mitchellh/cli"
+)
+
+func main() {
+
+	ui := &cli.BasicUi{
+		Reader:      os.Stdin,
+		Writer:      os.Stdout,
+		ErrorWriter: os.Stderr,
+	}
+
+	// CLI stuff
+	c := cli.NewCLI("ec2-snapper", "1.0.0")
+	c.Args = os.Args[1:]
+
+	c.Commands = map[string]cli.CommandFactory{
+		"create": func() (cli.Command, error) {
+			return &CreateCommand{
+				Ui:	&cli.ColoredUi{
+					Ui:	ui,
+					OutputColor: cli.UiColorNone,
+					ErrorColor:  cli.UiColorRed,
+					WarnColor:   cli.UiColorYellow,
+					InfoColor:   cli.UiColorGreen,
+				},
+			}, nil
+		},
+		"delete": func() (cli.Command, error) {
+			return &DeleteCommand{
+				Ui: &cli.ColoredUi{
+					Ui: ui,
+					OutputColor: cli.UiColorNone,
+					ErrorColor:  cli.UiColorRed,
+					WarnColor:   cli.UiColorYellow,
+					InfoColor:   cli.UiColorGreen,
+				},
+			}, nil
+		},
+	}
+
+	exitStatus, err := c.Run()
+	if err != nil {
+		fmt.Println(os.Stderr, err.Error())
+	}
+
+	os.Exit(exitStatus)
+
+}
