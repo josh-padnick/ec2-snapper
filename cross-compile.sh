1516635d1c0cb59b33624fbf56ffb914d65fea4e
Josh Padnick <josh.padnick@gmail.com>
2016-02-11 17:08:10 -0700

    Fix cross-compile script.

--- cross-compile.sh
+++ cross-compile.sh
@@ -4,32 +4,19 @@
 # Cross compile this go program for every major architecture
 #
 
-VERSION=0.3.0
+VERSION=v0.3.0
 
-GOOS=darwin
-GOARCH=386
-go build -v -o "${VERSION}/darwin_386/ec2-snapper" github.com/josh-padnick/ec2-snapper
+rm -Rf ${VERSION}
+rm "${VERSION}.zip"
 
-GOOS=darwin
-GOARCH=amd64
-go build -v -o "${VERSION}/darwin_amd64/ec2-snapper" github.com/josh-padnick/ec2-snapper
+env GOOS=darwin GOARCH=386 go build -v -o "${VERSION}/darwin_386/ec2-snapper" github.com/josh-padnick/ec2-snapper
+env GOOS=darwin GOARCH=amd64 go build -v -o "${VERSION}/darwin_amd64/ec2-snapper" github.com/josh-padnick/ec2-snapper
 
-GOOS=linux
-GOARCH=386
-go build -v -o "${VERSION}/linux_386/ec2-snapper" github.com/josh-padnick/ec2-snapper
+env GOOS=linux GOARCH=386 go build -v -o "${VERSION}/linux_386/ec2-snapper" github.com/josh-padnick/ec2-snapper
+env GOOS=linux GOARCH=amd64 go build -v -o "${VERSION}/linux_amd64/ec2-snapper" github.com/josh-padnick/ec2-snapper
+env GOOS=linux GOARCH=arm go build -v -o "${VERSION}/linux_arm/ec2-snapper" github.com/josh-padnick/ec2-snapper
 
-GOOS=linux
-GOARCH=amd64
-go build -v -o "${VERSION}/linux_amd64/ec2-snapper" github.com/josh-padnick/ec2-snapper
+env GOOS=windows GOARCH=386 go build -v -o "${VERSION}/windows_386/ec2-snapper.exe" github.com/josh-padnick/ec2-snapper
+env GOOS=windows GOARCH=amd64 go build -v -o "${VERSION}/windows_amd64/ec2-snapper.exe" github.com/josh-padnick/ec2-snapper
 
-GOOS=linux
-GOARCH=arm
-go build -v -o "${VERSION}/linux_arm/ec2-snapper" github.com/josh-padnick/ec2-snapper
-
-GOOS=windows
-GOARCH=386
-go build -v -o "${VERSION}/windows_386/ec2-snapper.exe" github.com/josh-padnick/ec2-snapper
-
-GOOS=windows
-GOARCH=amd64
-go build -v -o "${VERSION}/windows_amd64/ec2-snapper.exe" github.com/josh-padnick/ec2-snapper
+zip -r "${VERSION}.zip" "${VERSION}"
